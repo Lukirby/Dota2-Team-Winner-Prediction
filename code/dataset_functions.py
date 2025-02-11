461147fed4cde359ad5e9b365252053a45c91378
Cyclops <96382111+the-cyclops@users.noreply.github.com>
2025-02-11 20:12:09 +0100

    avg position and w-avg position added for each team

--- code/dataset_functions.py
+++ code/dataset_functions.py
@@ -216,3 +216,91 @@ def teamstats_teamheros_transform(df: pd.DataFrame, target: pd.DataFrame):
 
     return df,target
 
+def team_mean_position_transform (df: pd.DataFrame):
+    labels_radiant_x = ["r1_x", "r2_x", "r3_x", "r4_x", "r5_x"]
+    labels_radiant_y = ["r1_y", "r2_y", "r3_y", "r4_y", "r5_y"]
+    labels_dire_x = ["d1_x", "d2_x", "d3_x", "d4_x", "d5_x"]
+    labels_dire_y = ["d1_y", "d2_y", "d3_y", "d4_y", "d5_y"]
+
+    #calculate average x and y for Radiant team
+    df['radiant_avg_x'] = df[labels_radiant_x].mean(axis=1)
+    df['radiant_avg_y'] = df[labels_radiant_y].mean(axis=1)
+
+    #calculate average x and y for Dire team
+    df['dire_avg_x'] = df[labels_dire_x].mean(axis=1)
+    df['dire_avg_y'] = df[labels_dire_y].mean(axis=1)
+
+    return df
+
+def team_weighted_mean_position_transform(df: pd.DataFrame): 
+
+    labels_radiant_x = ["r1_x", "r2_x", "r3_x", "r4_x", "r5_x"]
+    labels_radiant_y = ["r1_y", "r2_y", "r3_y", "r4_y", "r5_y"]
+    labels_dire_x = ["d1_x", "d2_x", "d3_x", "d4_x", "d5_x"]
+    labels_dire_y = ["d1_y", "d2_y", "d3_y", "d4_y", "d5_y"]
+
+    df_Weighted = df.copy(deep=True)
+    df_Weighted  = get_average_distances(df_Weighted) 
+
+    distances_radiant = ["distance_r1", "distance_r2", "distance_r3", "distance_r4", "distance_r5"]
+    distances_dire = ["distance_d1", "distance_d2", "distance_d3", "distance_d4", "distance_d5"]
+    #calculate weights as the inverse of distances
+    weights_radiant = 1 / df_Weighted[distances_radiant]
+    weights_dire = 1 / df_Weighted[distances_dire]
+    
+    #calculate average x and y for Radiant team
+    #df['radiant_Weighted_avg_x'] = (df[labels_radiant_x] * weights_radiant).sum(axis=1) / weights_radiant.sum(axis=1)
+    #df['radiant_Weighted_avg_y'] = (df[labels_radiant_y] * weights_radiant).sum(axis=1) / weights_radiant.sum(axis=1)
+    #calculate average x and y for Dire team
+    #df['dire_Weighted_avg_x'] = (df[labels_dire_x] * weights_dire).sum(axis=1) / weights_dire.sum(axis=1)
+    #df['dire_Weighted_avg_y'] = (df[labels_dire_y] * weights_dire).sum(axis=1) / weights_dire.sum(axis=1)
+    #initialize weighted average columns
+    df['radiant_Weighted_avg_x'] = 0
+    df['radiant_Weighted_avg_y'] = 0
+    df['dire_Weighted_avg_x'] = 0
+    df['dire_Weighted_avg_y'] = 0
+    for i in range(5):
+        #calculate weighted average x and y for Radiant team
+        df['radiant_Weighted_avg_x'] += df[labels_radiant_x[i]] * weights_radiant.iloc[:, i]
+        df['radiant_Weighted_avg_y'] += df[labels_radiant_y[i]] * weights_radiant.iloc[:, i]
+
+        #calculate weighted average x and y for Dire team
+        df['dire_Weighted_avg_x'] += df[labels_dire_x[i]] * weights_dire.iloc[:, i]
+        df['dire_Weighted_avg_y'] += df[labels_dire_y[i]] * weights_dire.iloc[:, i]
+
+    #normalize by the sum of weights
+    df['radiant_Weighted_avg_x'] /= weights_radiant.sum(axis=1)
+    df['radiant_Weighted_avg_y'] /= weights_radiant.sum(axis=1)
+    df['dire_Weighted_avg_x'] /= weights_dire.sum(axis=1)
+    df['dire_Weighted_avg_y'] /= weights_dire.sum(axis=1)
+    return df
+
+#this is ok, tested
+def get_average_distances(df: pd.DataFrame):
+    labels_radiant_x = ["r1_x", "r2_x", "r3_x", "r4_x", "r5_x"]
+    labels_radiant_y = ["r1_y", "r2_y", "r3_y", "r4_y", "r5_y"]
+    labels_dire_x = ["d1_x", "d2_x", "d3_x", "d4_x", "d5_x"]
+    labels_dire_y = ["d1_y", "d2_y", "d3_y", "d4_y", "d5_y"]
+
+    radiant_distances = calculate_distances(df, labels_radiant_x, labels_radiant_y)
+    dire_distances = calculate_distances(df, labels_dire_x, labels_dire_y)
+
+    for label in radiant_distances:
+        truncated_label = label[:-2]
+        df[f'distance_{truncated_label}'] = radiant_distances[label]
+
+    for label in dire_distances:
+        truncated_label = label[:-2]
+        df[f'distance_{truncated_label}'] = dire_distances[label]
+
+    return df
+#this is ok, tested
+def calculate_distances(df: pd.DataFrame, x_labels, y_labels):
+    distances = {label: [] for label in x_labels}
+    for i in range(len(x_labels)):
+        for j in range(len(x_labels)):
+            if i != j:
+                dist = np.sqrt((df[x_labels[i]] - df[x_labels[j]])**2 + (df[y_labels[i]] - df[y_labels[j]])**2)
+                distances[x_labels[i]].append(dist)
+    return {label: np.mean(distances[label], axis=0) for label in distances}
+
